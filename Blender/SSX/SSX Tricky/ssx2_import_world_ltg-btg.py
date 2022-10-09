743ee4e635fe47f1c231f006c3b99e9d191bd2a3
Linkz <39253822+Linkz64@users.noreply.github.com>
2022-10-09 11:42:11 +0100

    Added Address to custom properties
    Fine name as object prefix

--- Blender/SSX/SSX Tricky/ssx2_import_world_ltg-btg.py
+++ Blender/SSX/SSX Tricky/ssx2_import_world_ltg-btg.py
@@ -1,6 +1,6 @@
 import bpy
 import struct
-import mathutils
+from mathutils import Vector
 
 import time
 
@@ -10,15 +10,22 @@ collec = bpy.context.collection
 timeStart = time.time() # start timer
 print(f"TIME STARTED {time.localtime().tm_hour}:{time.localtime().tm_min}")
 
-def read_bounds():
 
-    filePath = "G:/Emulated/Playstation 2/Games/SSX Tricky/data/models/"
-    fileName = "gari"+".ltg"
-    f = open(filePath+"/"+fileName, 'rb')
+filePath = "G:/Emulated/Playstation 2/Games/SSX Tricky/data/models"
+fileName = "snow"
+fileSuff = "ltg"
+
+scaleDiv = 100
+
+
+
+def import_bounds():
+
+    f = open(filePath+"/"+fileName+"."+fileSuff, 'rb')
 
     en = "<"
 
-    f.seek(0x3,  0)
+    f.seek(0x3, 0)
     (enByte,) = struct.unpack('B', f.read(1))
 
     if enByte == 1:
@@ -32,18 +39,18 @@ def read_bounds():
 
     print(worldBounds)
 
-    create_all("bbox_world", worldBounds[0], worldBounds[1], worldBounds[2])
+    create_all(f"{fileName}_bbox.world", worldBounds[0], worldBounds[1], worldBounds[2])
 
     f.seek(0x2C, 0)
 
-    (offsetCount    ,) = struct.unpack(en+'I', f.read(4))
-    (offsetListCount,) = struct.unpack(en+'I', f.read(4))
+    offsetCount     = struct.unpack(en+'I', f.read(4))[0]
+    offsetListCount = struct.unpack(en+'I', f.read(4))[0]
 
     f.seek(0x48, 0)
 
-    (gridBoxCount    ,) = struct.unpack(en+'I', f.read(4)) # = 16
-    (offsetListOffset,) = struct.unpack(en+'I', f.read(4)) # = 84
-    (offsetListEnd   ,) = struct.unpack(en+'I', f.read(4))
+    gridBoxCount     = struct.unpack(en+'I', f.read(4))[0] # = 16
+    offsetListOffset = struct.unpack(en+'I', f.read(4))[0] # = 84
+    offsetListEnd    = struct.unpack(en+'I', f.read(4))[0]
 
     f.seek(offsetListOffset, 0)
 
@@ -54,12 +61,15 @@ def read_bounds():
         offsetListContainer.append(offsetTuple)
 
 
-    sortedOffsetList = clear_list(offsetListContainer)
+    sortedOffsetList = clean_list(offsetListContainer)
 
 
     for i in range(len(sortedOffsetList)):
 
         f.seek(sortedOffsetList[i], 0)
+
+        curAddress = str(hex(f.tell()))
+        objName = f"{fileName}_bbox.a_{i}"
 
         firstBounds = [(0,0,0)]*3
         for j in range(3):
@@ -67,13 +77,20 @@ def read_bounds():
 
         firstExtras = struct.unpack(en+'I'*12, f.read(4*12))
     
-        #create_all(f"bbox_{i}_a", firstBounds[0], firstBounds[1], firstBounds[2])
-        create_bounds(f"bbox_{i}_a", firstBounds[0], firstBounds[1])
+        #create_all(f"bbox.a_{i}", firstBounds[0], firstBounds[1], firstBounds[2])
+        create_bounds(objName, firstBounds[0], firstBounds[1])
 
-        print(f"\nbbox_{i}_a    Ends:{hex(f.tell())}    Values:\n{firstBounds}\n{firstExtras}\n")
+        curObj = bdata.objects[objName]
+        curObj["Address: 0x"] = curAddress[2:]
+
+
+        print(f"\n{objName}    Ends:{hex(f.tell())}    Values:\n{firstBounds}\n{firstExtras}\n")
 
 
         for j in range(gridBoxCount):
+
+            curAddress = str(hex(f.tell()))
+            obj2Name = f"{fileName}_bbox.b_{i}_{j}"
 
             secondBounds = [(0,0,0)]*3
             for k in range(3):
@@ -81,10 +98,13 @@ def read_bounds():
 
             secondExtras = struct.unpack(en+'I'*10, f.read(4*10))
     
-            #create_all(f"bbox_b_{i}_{j}", secondBounds[0], secondBounds[1], secondBounds[2])
-            create_bounds(f"bbox_{i}_b_{j}", secondBounds[0], secondBounds[1])
+            #create_all(f"bbox.b_{i}_{j}", secondBounds[0], secondBounds[1], secondBounds[2])
+            create_bounds(f"{obj2Name}", secondBounds[0], secondBounds[1])
+
+            curObj = bdata.objects[obj2Name]
+            curObj["Address: 0x"] = curAddress[2:]
     
-            print(f"bbox_{i}_b_{j}    Ends:{hex(f.tell())}    Values:\n{secondBounds}\n{secondExtras}")
+            print(f"{obj2Name}    Ends:{hex(f.tell())}    Values:\n{secondBounds}\n{secondExtras}")
     
     
     timeEnd = time.time()
@@ -93,29 +113,12 @@ def read_bounds():
     f.close()
 
 
-
-def clear_list(messyList): # removes 0's from list
-    clearedList = []
-    for i in range(len(messyList)):
-        for j in range(len(messyList[0])): # remove null offsets
-            if messyList[i][j] > 0:
-                clearedList.append(messyList[i][j])
-    return clearedList
-
-def create_empty(name, pos): # create empty object
-    empty = bdata.objects.new(name, None) # Object instances
-    collec.objects.link(empty)
-    empty.empty_display_type = 'ARROWS'
-    empty.empty_display_size = 250
-    empty.show_name = True
-    empty.location = pos
-
 def create_bounds(name, pos1, pos2): # create just bounds
 
     mesh = bdata.meshes.new(name)
     obj = bdata.objects.new(name, mesh)
     collec.objects.link(obj)
-    mesh.from_pydata([pos1, pos2], [], [])
+    mesh.from_pydata([Vector(pos1)/scaleDiv, Vector(pos2)/scaleDiv], [], [])
     obj.display_type = 'BOUNDS'
 
 def create_all(name, pos0, pos1, pos2): # create bounds with 3rd point as object origin
@@ -123,14 +126,14 @@ def create_all(name, pos0, pos1, pos2): # create bounds with 3rd point as object
     mesh = bdata.meshes.new(name)
     obj = bdata.objects.new(name, mesh)
     collec.objects.link(obj)
-    mesh.from_pydata([ pos0, pos1 ], [], [])
+    mesh.from_pydata([Vector(pos0)/scaleDiv, Vector(pos1)/scaleDiv], [], [])
     obj.display_type = 'BOUNDS'
 
-    bpy.context.scene.cursor.location = pos2        # moves cursor to pos0
-    obj.select_set(True)                            # selects object
-    bpy.ops.object.origin_set(type='ORIGIN_CURSOR') # sets object origin to cursor
-    bpy.context.scene.cursor.location = (0,0,0)     # moves cursor to world origin
-    obj.select_set(False)                           # deselects object
+    bpy.context.scene.cursor.location = Vector(pos2)/scaleDiv # moves cursor to pos0
+    obj.select_set(True)                                      # selects object
+    bpy.ops.object.origin_set(type='ORIGIN_CURSOR')           # sets object origin to cursor
+    bpy.context.scene.cursor.location = (0,0,0)               # moves cursor to world origin
+    obj.select_set(False)                                     # deselects object
 
 
 
@@ -146,4 +149,4 @@ def run_wout_update(func):              # run without view update
         _BPyOpsSubModOp._view_layer_update = view_layer_update
 
 
-run_wout_update(read_bounds)
+run_wout_update(import_bounds)
