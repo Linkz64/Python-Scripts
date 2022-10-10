c25463dd485edc843ad2f42f320c8516ad3b290d
Linkz <39253822+Linkz64@users.noreply.github.com>
2022-10-10 09:33:31 +0100

    Update ssx2_import_world_pbd_splines.py

--- Blender/SSX/SSX Tricky/ssx2_import_world_pbd_splines.py
+++ Blender/SSX/SSX Tricky/ssx2_import_world_pbd_splines.py
@@ -16,7 +16,7 @@ print(f"TIME STARTED {time.localtime().tm_hour}:{time.localtime().tm_min}")
 
 filePath = "G:/Emulated/Playstation 2/Games/SSX Tricky/data/models"
 fileName = "gari"
-fileSuff = "pbd" # ps2 = .pbd, xbox = xbd, ngc = nbd ( ONLY TESTED ON PBD )
+fileSuff = "pbd" # ps2 = .pbd, xbox = .xbd, ngc = .nbd
 
 
 scaleDiv = 100 # 100 for Blender resize, 1 for SSX default
@@ -31,26 +31,35 @@ def import_splines():
 
 
     f.seek(0x3, 0)
-    enByte = struct.unpack('B', f.read(1))[0]
+    platformID = struct.unpack('B', f.read(1))[0]
 
     en = "<"
-    if enByte == 3: # 1 = ps2, 2 = xbox, 3 = ngc
+    countOffset  = 0x20
+    offsetOffset = 0x5C
+
+    if   platformID == 2:      # 1 = ps2, 2 = xbox, 3 = ngc
+        countOffset  = 0x28
+        offsetOffset = 0x70
+
+    elif platformID == 3:
         en = ">"
 
 
-    f.seek(0x20, 0)
+    f.seek(countOffset, 0)
 
     splineCount = struct.unpack(en+'I', f.read(4))[0]
     splineSegmentCount = struct.unpack(en+'I', f.read(4))[0]
 
-    f.seek(0x5C, 0)
+
+    f.seek(offsetOffset, 0)
 
     splineOffset = struct.unpack(en+'I', f.read(4))[0]
     splineSegmentOffset = struct.unpack(en+'I', f.read(4))[0]
 
 
-
     #splineCount = 8
+
+
 
 
 
@@ -89,12 +98,11 @@ def import_splines():
         splineSegmentList.append(splineSegment)
 
 
-        splineSegmentPointList.append(
-            (
+        splineSegmentPointList.append((
                 Vector((splineSegment["point1"][0],splineSegment["point1"][1],splineSegment["point1"][2])),
                 Vector((splineSegment["point2"][0],splineSegment["point2"][1],splineSegment["point2"][2])),
                 Vector((splineSegment["point3"][0],splineSegment["point3"][1],splineSegment["point3"][2])),
-                Vector((splineSegment["point4"][0],splineSegment["point4"][1],splineSegment["point4"][2])),
+                Vector((splineSegment["point4"][0],splineSegment["point4"][1],splineSegment["point4"][2]))
             )
         )
 
@@ -150,28 +158,37 @@ def import_splines():
             boundsName = f"{fileName}_spline.{i}_bounds"
     
             create_bounds(boundsName, spline["bboxPt1"], spline["bboxPt2"])
-    
-            boundsObj = bdata.objects[boundsName]
-    
-            boundsObj["Address 0x"]   = curAddress[2:]
-            boundsObj["unkInt1"]      = spline["unkInt1"]
-            boundsObj["segmentCount"] = spline["segmentCount"]
-            boundsObj["segmentIndex"] = spline["segmentIndex"]
-            boundsObj["unkInt2"]      = spline["unkInt2"]
 
 
-        coordsList = []
+
+        curveName = f'spline{i}'
+
+        curve = bpy.data.curves.new(curveName+"_NURBS", "CURVE")
+
         for j in range(spline["segmentCount"]):
-            curSegCoords = decode_curve_points(  splineSegmentPointList[ spline["segmentIndex"]+j ]  )
+            coords = decode_curve_points(  splineSegmentPointList[ spline["segmentIndex"]+j ]  )
 
-            coordsList.append(curSegCoords[0])
-            coordsList.append(curSegCoords[1])
-            coordsList.append(curSegCoords[2])
-            coordsList.append(curSegCoords[3])
 
-        print(coordsList)
+            cspline = curve.splines.new('NURBS')
 
-        create_curve_obj(coordsList, f'curve{i}')
+            curve.dimensions = '3D'
+
+            cspline.points.add(len(coords)-1) # already contains a point that's why it's adding 1 less than total
+            for (index, point) in enumerate(coords):
+                cspline.points[index].co = (point[0]/scaleDiv, point[1]/scaleDiv, point[2]/scaleDiv, 1.0)
+
+            cspline.use_endpoint_u = True
+
+
+        curveObj = bpy.data.objects.new(curveName, curve)
+        collecMain.objects.link(curveObj)
+
+    
+        curveObj["Address 0x"]   = curAddress[2:]
+        curveObj["unkInt1"]      = spline["unkInt1"]
+        curveObj["segmentCount"] = spline["segmentCount"]
+        curveObj["segmentIndex"] = spline["segmentIndex"]
+        curveObj["unkInt2"]      = spline["unkInt2"]
 
 
     timeEnd = time.time()
@@ -185,12 +202,13 @@ def import_splines():
 
 
 
-# decoding with equations
 
-def decode_curve_points(encPoints):
+
+
+def decode_curve_points(encPoints): # decoding with equations
 
     midPoints = [Vector((0.0, 0.0, 0.0))]*4
-    rawPoints = [Vector((0.0, 0.0, 0.0))]*4
+    decPoints = [Vector((0.0, 0.0, 0.0))]*4
 
 
     midPoints[0] =  encPoints[0]
@@ -198,86 +216,20 @@ def decode_curve_points(encPoints):
     midPoints[2] = (encPoints[2]   + encPoints[1])/3 + midPoints[1]
     midPoints[3] =  encPoints[3]   + encPoints[2]    + encPoints[1] + encPoints[0]
 
-    rawPoints[0] = midPoints[0]
-    rawPoints[1] = midPoints[1]
-    rawPoints[2] = midPoints[2]
-    rawPoints[3] = midPoints[3]
+    decPoints[0] = midPoints[0]
+    decPoints[1] = midPoints[1]
+    decPoints[2] = midPoints[2]
+    decPoints[3] = midPoints[3]
 
-    return rawPoints
-
-
+    return decPoints
 
 
 
 
 
-# generate spline curve segment
-
-def create_curve_obj(coords, name):
-
-    cu = bpy.data.curves.new(name+"_NURBS", "CURVE")
-    cpath = cu.splines.new('NURBS')  # 'POLY''BEZIER''BSPLINE''CARDINAL''NURBS'
-
-    cu.dimensions = '3D'
-
-    if cpath.type in ['NURBS', 'POLY']:
-        cpath.points.add(len(coords)-1)
-        for (index, point) in enumerate(coords):
-            cpath.points[index].co = (point[0]/scaleDiv, point[1]/scaleDiv, point[2]/scaleDiv, 1.0)
-
-            #create_empty(name, (point[0]/scaleDiv, point[1]/scaleDiv, point[2]/scaleDiv))
-
-            #print(cpath.points[index].co)
-
-        cpath.use_endpoint_u = True
-
-
-    elif cpath.type in ['BEZIER']:             # don't use this
-        cpath.bezier_points.add(len(coords)-1)
-        for (index, point) in enumerate(coords):
-            x, y, z = point
-            cpath.bezier_points[index].co = x, y, z
-            cpath.bezier_points[index].handle_left = x-5, y-5, z-5
-            cpath.bezier_points[index].handle_right = x+5, y+5, z+5
-            print(cpath.bezier_points[index].co)
-
-    ob = bpy.data.objects.new(name, cu)
-    bpy.context.collection.objects.link(ob)
 
 
 
-"""
-
-def create_curve_obj(coords, name):
-    curveData = bpy.data.curves.new(name+"_NURBS", type='CURVE')
-    curveData.dimensions = '3D'
-    curveData.resolution_u = 2
-    
-    polyline = curveData.splines.new('NURBS')
-
-    polyline.points.add(len(coords))
-    for i, coord in enumerate(coords):
-        x,y,z = coord
-        polyline.points[i].co = (x/scaleDiv, y/scaleDiv, z/scaleDiv, 1)
-        print(polyline.points[i].co)
-    polyline.use_endpoint_u = True
-    
-
-    curveOB = bpy.data.objects.new(name, curveData)
-    bpy.context.collection.objects.link(curveOB)
-
-"""
-
-
-
-
-def create_empty(name, pos): # create empty object
-    empty = bdata.objects.new(name, None) # Object instances
-    collecMain.objects.link(empty)
-    empty.empty_display_type = 'ARROWS'
-    #empty.empty_display_size = 250
-    empty.show_name = True
-    empty.location = pos
 
 def create_bounds(name, pos1, pos2): # create just bounds
 
