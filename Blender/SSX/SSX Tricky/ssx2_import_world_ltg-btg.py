019bf7aa4534f03a884fffc9deaeba52094be49f
Linkz <39253822+Linkz64@users.noreply.github.com>
2022-10-12 11:49:34 +0100

    Added custom properties with counts and offsets

--- Blender/SSX/SSX Tricky/ssx2_import_world_ltg-btg.py
+++ Blender/SSX/SSX Tricky/ssx2_import_world_ltg-btg.py
@@ -12,16 +12,16 @@ print(f"TIME STARTED {time.localtime().tm_hour}:{time.localtime().tm_min}")
 
 
 filePath = "G:/Emulated/Playstation 2/Games/SSX Tricky/data/models"
-fileName = "snow"
+fileName = "trick"
 fileSuff = "ltg"
 
 scaleDiv = 100
+globalOffsets = True
 
-
+f = open(filePath+"/"+fileName+"."+fileSuff, 'rb')
 
 def import_bounds():
 
-    f = open(filePath+"/"+fileName+"."+fileSuff, 'rb')
 
     en = "<"
 
@@ -68,7 +68,7 @@ def import_bounds():
 
         f.seek(sortedOffsetList[i], 0)
 
-        curAddress = str(hex(f.tell()))
+        mainBoxAddress = f.tell()
         objName = f"{fileName}_bbox.a_{i}"
 
         firstBounds = [(0,0,0)]*3
@@ -81,7 +81,7 @@ def import_bounds():
         create_bounds(objName, firstBounds[0], firstBounds[1])
 
         curObj = bdata.objects[objName]
-        curObj["Address: 0x"] = curAddress[2:]
+        curObj["Address: 0x"] = str(hex(mainBoxAddress)[2:])
 
 
         print(f"\n{objName}    Ends:{hex(f.tell())}    Values:\n{firstBounds}\n{firstExtras}\n")
@@ -89,22 +89,53 @@ def import_bounds():
 
         for j in range(gridBoxCount):
 
-            curAddress = str(hex(f.tell()))
+            curAddress = hex(f.tell())
             obj2Name = f"{fileName}_bbox.b_{i}_{j}"
 
             secondBounds = [(0,0,0)]*3
             for k in range(3):
                 secondBounds[k] = struct.unpack(en+'fff', f.read(4*3))
 
-            secondExtras = struct.unpack(en+'I'*10, f.read(4*10))
+            indexCounts = struct.unpack(en+'H'*8, f.read(2*8)) # patch, instance, unk, spline, light, unk2, particle, unk3
+            indexOffset = struct.unpack(en+'I'*6, f.read(4*6)) # ^ offsets for these ^
+
     
             #create_all(f"bbox.b_{i}_{j}", secondBounds[0], secondBounds[1], secondBounds[2])
             create_bounds(f"{obj2Name}", secondBounds[0], secondBounds[1])
 
+
+            indexOffsetList = [0]*len(indexOffset)
+            for k in range(len(indexOffset)): # reformatted for custom properties
+
+                if globalOffsets == True:
+
+                    if mainBoxAddress+indexOffset[k] != mainBoxAddress:
+                        indexOffsetList[k] = mainBoxAddress+indexOffset[k]
+
+                    elif mainBoxAddress+indexOffset[k] == mainBoxAddress:
+                        indexOffsetList[k] = indexOffset[k]
+                else:
+                    indexOffsetList[k] = indexOffset[k]
+
             curObj = bdata.objects[obj2Name]
-            curObj["Address: 0x"] = curAddress[2:]
+            curObj["00|   Address: 0x"] = str(curAddress[2:])
+
+            curObj["01|   Patches"]   = indexCounts[0]
+            curObj["02|   Instances"] = indexCounts[1]
+            curObj["03|   Unknown1"]  = indexCounts[2]
+            curObj["04|   Splines"]   = indexCounts[3]
+            curObj["05|   Lights"]    = indexCounts[4]
+            curObj["06|   Unknown2"]  = indexCounts[5]
+            curObj["07|   Particles"] = indexCounts[6]
+            curObj["08|   Unknown3"]  = indexCounts[7]
+            curObj["09|   Patch: 0x"] = str( hex(indexOffsetList[0])[2:])
+            curObj["10|   Unk    0x"] = str( hex(indexOffsetList[1])[2:])
+            curObj["11|   Unk    0x"] = str( hex(indexOffsetList[2])[2:])
+            curObj["12|   Unk    0x"] = str( hex(indexOffsetList[3])[2:])
+            curObj["13|   Unk    0x"] = str( hex(indexOffsetList[4])[2:])
+            curObj["14|   Unk    0x"] = str( hex(indexOffsetList[5])[2:])
     
-            print(f"{obj2Name}    Ends:{hex(f.tell())}    Values:\n{secondBounds}\n{secondExtras}")
+            print(f"{obj2Name}    Ends:{hex(f.tell())}    Values:\n{secondBounds}")
     
     
     timeEnd = time.time()
@@ -112,6 +143,29 @@ def import_bounds():
 
     f.close()
 
+
+def unpack_bbox():
+    a = [Vector(0,0,0)]*3
+    for i in range(3):
+        a[i] = struct.unpack(en+'fff', f.read(4*3))
+    return a
+
+
+def clean_list(messyList): # removes 0's from list
+    cleanedList = []
+    for i in range(len(messyList)):
+        for j in range(len(messyList[0])): # remove null offsets
+            if messyList[i][j] > 0:
+                cleanedList.append(messyList[i][j])
+    return cleanedList
+
+def create_empty(name, pos): # create empty object
+    empty = bdata.objects.new(name, None) # Object instances
+    collec.objects.link(empty)
+    empty.empty_display_type = 'ARROWS'
+    empty.empty_display_size = 250
+    empty.show_name = True
+    empty.location = pos
 
 def create_bounds(name, pos1, pos2): # create just bounds
 
